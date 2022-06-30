ecfa05a96f3cbcc89a673cd4ebf959d4fbdc9658
LittleCodee <1109069349@qq.com>
2022-06-30 09:30:39 +0800

    动画
    给div增加了动画效果

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    let div = document.querySelector('div');
+    div.onclick = function(){
+        if(div.clientWidth == 100){
+            div.style.width = '200px';
+            div.style.height = '500px';
+        }else if(div.clientWidth == 200){
+            div.style.width = '300px';
+            div.style.height = '300px';
+        }else if(div.clientWidth == 300){
+            div.style.width = '400px';
+            div.style.height = '100px';
+        }else if(div.clientWidth == 400){
+            div.style.width = '100px';
+            div.style.height = '400px';
+        }
+    }
+}
