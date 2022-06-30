a97565e0eedb64514902015c7267334dc3be2a26
LittleCodee <1109069349@qq.com>
2022-06-30 10:01:09 +0800

    动画？
    动画！

--- js/index.js
+++ js/index.js
@@ -3,16 +3,16 @@ window.onload = function(){
     div.onclick = function(){
         if(div.clientWidth == 100){
             div.style.width = '200px';
-            div.style.height = '500px';
+            div.style.height = '200px';
         }else if(div.clientWidth == 200){
             div.style.width = '300px';
             div.style.height = '300px';
         }else if(div.clientWidth == 300){
             div.style.width = '400px';
-            div.style.height = '100px';
+            div.style.height = '400px';
         }else if(div.clientWidth == 400){
             div.style.width = '100px';
-            div.style.height = '400px';
+            div.style.height = '100px';
         }
     }
 }
